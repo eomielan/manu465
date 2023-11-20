df74e1bfa948419d0cf0fbd66235968fd9a0cad4
eomielan <eomielan@gmail.com>
2023-11-19 18:57:04 -0800

    Update alphanum script so colours work on windows

--- project/alphanum_generator.py
+++ project/alphanum_generator.py
@@ -1,11 +1,9 @@
 import os
 import time
 
+from colorama import Fore, Style, init
+
 # Constants
-YELLOW_CODE = "\033[93m"
-RED_CODE = "\033[91m"
-GREEN_CODE = "\033[92m"
-RESET_CODE = "\033[0m"
 STRING_LENGTH = 8
 SLEEP_TIME = 2  # seconds
 
@@ -30,21 +28,22 @@ def print_diff(expected: str, actual: str) -> None:
         expected (str): The expected string.
         actual (str): The actual string.
     """
-    print("Expected: " + YELLOW_CODE + expected + RESET_CODE)
+    print("Expected: " + Fore.YELLOW + expected + Style.RESET_ALL)
     print("Actual:   ", end="")
 
     for i in range(min(len(expected), len(actual))):
         if expected[i] == actual[i]:
-            print(GREEN_CODE + actual[i] + RESET_CODE, end="")
+            print(Fore.GREEN + actual[i] + Style.RESET_ALL, end="")
         else:
-            print(RED_CODE + actual[i] + RESET_CODE, end="")
+            print(Fore.RED + actual[i] + Style.RESET_ALL, end="")
 
     # Print any remaining characters in the input string, if any are present
     if len(actual) > len(expected):
-        print(RED_CODE + actual[len(expected):] + RESET_CODE, end="")
+        print(Fore.RED + actual[len(expected):] + Style.RESET_ALL, end="")
 
 
 def main():
+    init()  # Initialize colorama
     os.system("cls" if os.name == "nt" else "clear")  # Clear screen
 
     while True:
@@ -52,7 +51,7 @@ def main():
         os.system("cls" if os.name == "nt" else "clear")
 
         expected = generate(STRING_LENGTH)
-        print(YELLOW_CODE + expected + RESET_CODE)
+        print(Fore.YELLOW + expected + Style.RESET_ALL)
 
         time.sleep(SLEEP_TIME)
         os.system("cls" if os.name == "nt" else "clear")
